e17c04ce74d3a68c03f45288b751bf16baee5e51
CorpusFrisky <robert.katz33@gmail.com>
2015-02-14 20:02:58 -0800

    Forgot a file.

---
+++ CorpusFrisky.VisualSynth.SynthModules/DependencyInjection/SynthModulesDI.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using CorpusFrisky.VisualSynth.SynthModules.Models.ShapeGenerators;
+using CorpusFrisky.VisualSynth.SynthModules.ViewModels.ShapeGenerators;
+using CorpusFrisky.VisualSynth.SynthModules.Views.ShapeGenerators;
+
+namespace CorpusFrisky.VisualSynth.SynthModules.DependencyInjection
+{
+    public class SynthModulesDI : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterType<TriangleGenerator>().AsSelf();
+            builder.RegisterType<TriangleGeneratorView>().AsSelf().SingleInstance();
+            builder.RegisterType<TriangleGeneratorViewModel>().AsSelf();
+        }
+    }
+}
