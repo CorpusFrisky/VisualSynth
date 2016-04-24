795fa764f055e15752c30ee99fbd3d6834023f01
CorpusFrisky <robert.katz33@gmail.com>
2016-04-23 18:24:58 -0700

    A little more work on the color inverter module.

--- CorpusFrisky.VisualSynth.SynthModules/ViewModels/FrameEffects/ColorInverterViewModel.cs
+++ CorpusFrisky.VisualSynth.SynthModules/ViewModels/FrameEffects/ColorInverterViewModel.cs
@@ -13,9 +13,10 @@ using System.Linq;
 
 namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
 {
-    public class ColorInverterViewModel : SynthModuleBaseViewModel
+    public class ColorInverterViewModel : SynthModuleBaseViewModel, IDisposable
     {
-        private int _bufferId;
+        private int _inputFramebufferId;
+        private int _outputFramebufferId;
         private int _colorTextureId;
         private int _depthTextureId;
 
@@ -31,6 +32,14 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
             SetupPins();
         }
 
+        //TODO: stick this in a base class
+        public void Dispose()
+        {
+            GL.DeleteTextures(1, ref _colorTextureId);
+            GL.DeleteTextures(1, ref _depthTextureId);
+            GL.Ext.DeleteFramebuffers(1, ref _inputFramebufferId);
+        }
+
         protected override void SetupPins()
         {
             InputPins.Add(new InputValuePin
@@ -44,7 +53,7 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
             OutputPins.Add(new OutputFramePin()
             {
                 Module = this,
-                GetBufferId_Function = () => BufferId,
+                GetBufferId_Function = () => FramebufferId,
             });
         }
 
@@ -52,7 +61,7 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
 
         private List<OutputHybridPin> SourcePins { get; set; }
 
-        private int BufferId => _bufferId;
+        private int FramebufferId => _outputFramebufferId;
 
         private bool TexturesAndBufferAreInitialized { get; set; }
 
@@ -69,7 +78,17 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
 
             if (!TexturesAndBufferAreInitialized)
             {
-                InitializeTexturesAndBuffer();
+                InitializeTexturesAndFramebuffer();
+            }
+            else
+            {
+                // Create a FBO and attach the textures
+                GL.Ext.GenFramebuffers(1, out _inputFramebufferId);
+                GL.Ext.BindFramebuffer(FramebufferTarget.FramebufferExt, _inputFramebufferId);
+                GL.Ext.FramebufferTexture2D(FramebufferTarget.FramebufferExt, FramebufferAttachment.ColorAttachment0Ext, TextureTarget.Texture2D, _colorTextureId, 0);
+                GL.Ext.FramebufferTexture2D(FramebufferTarget.FramebufferExt, FramebufferAttachment.DepthAttachmentExt, TextureTarget.Texture2D, _depthTextureId, 0);
+
+                CheckForErrorsInitializingTexturesAndFramebuffer();
             }
 
             if (SourcePins.Any(x => x.IsOutputRendered))
@@ -79,8 +98,31 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
             }
             else
             {
-                var commandList = SourcePins.SelectMany(x => x.CommandListOutput);
-                
+                GL.PushAttrib(AttribMask.ViewportBit);
+                {
+                    GL.Viewport(0, 0, GlobalValues.DisplayWidth, GlobalValues.DisplayHeight);
+
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    //Do all the rendering leading up to this module into our inputframebuffer
+                    var commandList = SourcePins.SelectMany(x => x.CommandListOutput);
+                    foreach(var command in commandList)
+                    {
+                        command.Invoke(true);
+                    }
+                }
+                GL.PopAttrib();
+                GL.Ext.BindFramebuffer(FramebufferTarget.FramebufferExt, 0); // disable rendering into the FBO
+
+                GL.ClearColor(.1f, .2f, .3f, 0f);
+                GL.Color3(1f, 1f, 1f);
+
+                GL.Enable(EnableCap.Texture2D); // enable Texture Mapping
+                GL.BindTexture(TextureTarget.Texture2D, 0); // bind default texture
+
+                //Now draw this framebuffer into the output framebuffer using the effecting shader
+
+                //The module connected to our output will then render the output framebuffer as a texture
             }
         }
 
@@ -120,7 +162,9 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
 
         #region Helper Methods
 
-        private void InitializeTexturesAndBuffer()
+        //TODO: Make these inheritable by all modules that could generate frames
+
+        private void InitializeTexturesAndFramebuffer()
         {
             GL.Enable(EnableCap.DepthTest);
             GL.ClearDepth(1.0f);
@@ -149,13 +193,86 @@ namespace CorpusFrisky.VisualSynth.SynthModules.ViewModels.FrameEffects
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToBorder);
 
             // Create a FBO and attach the textures
-            GL.Ext.GenFramebuffers(1, out _bufferId);
-            GL.Ext.BindFramebuffer(FramebufferTarget.FramebufferExt, _bufferId);
+            GL.Ext.GenFramebuffers(1, out _inputFramebufferId);
+            GL.Ext.BindFramebuffer(FramebufferTarget.FramebufferExt, _inputFramebufferId);
             GL.Ext.FramebufferTexture2D(FramebufferTarget.FramebufferExt, FramebufferAttachment.ColorAttachment0Ext, TextureTarget.Texture2D, _colorTextureId, 0);
             GL.Ext.FramebufferTexture2D(FramebufferTarget.FramebufferExt, FramebufferAttachment.DepthAttachmentExt, TextureTarget.Texture2D, _depthTextureId, 0);
 
+            CheckForErrorsInitializingTexturesAndFramebuffer();
+        }
+
+        private void CheckForErrorsInitializingTexturesAndFramebuffer()
+        { 
+            //TODO: log instead of console write
+            switch (GL.Ext.CheckFramebufferStatus(FramebufferTarget.FramebufferExt))
+            {
+                case FramebufferErrorCode.FramebufferCompleteExt:
+                    {
+                        Console.WriteLine("FBO: The framebuffer is complete and valid for rendering.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferIncompleteAttachmentExt:
+                    {
+                        Console.WriteLine("FBO: One or more attachment points are not framebuffer attachment complete. This could mean there’s no texture attached or the format isn’t renderable. For color textures this means the base format must be RGB or RGBA and for depth textures it must be a DEPTH_COMPONENT format. Other causes of this error are that the width or height is zero or the z-offset is out of range in case of render to volume.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferIncompleteMissingAttachmentExt:
+                    {
+                        Console.WriteLine("FBO: There are no attachments.");
+                        break;
+                    }
+                /* case  FramebufferErrorCode.GL_FRAMEBUFFER_INCOMPLETE_DUPLICATE_ATTACHMENT_EXT: 
+                     {
+                         Console.WriteLine("FBO: An object has been attached to more than one attachment point.");
+                         break;
+                     }*/
+                case FramebufferErrorCode.FramebufferIncompleteDimensionsExt:
+                    {
+                        Console.WriteLine("FBO: Attachments are of different size. All attachments must have the same width and height.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferIncompleteFormatsExt:
+                    {
+                        Console.WriteLine("FBO: The color attachments have different format. All color attachments must have the same format.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferIncompleteDrawBufferExt:
+                    {
+                        Console.WriteLine("FBO: An attachment point referenced by GL.DrawBuffers() doesn’t have an attachment.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferIncompleteReadBufferExt:
+                    {
+                        Console.WriteLine("FBO: The attachment point referenced by GL.ReadBuffers() doesn’t have an attachment.");
+                        break;
+                    }
+                case FramebufferErrorCode.FramebufferUnsupportedExt:
+                    {
+                        Console.WriteLine("FBO: This particular FBO configuration is not supported by the implementation.");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("FBO: Status unknown. (yes, this is really bad.)");
+                        break;
+                    }
+            }
+
+            // using FBO might have changed states, e.g. the FBO might not support stereoscopic views or double buffering
+            int[] queryinfo = new int[6];
+            GL.GetInteger(GetPName.MaxColorAttachmentsExt, out queryinfo[0]);
+            GL.GetInteger(GetPName.AuxBuffers, out queryinfo[1]);
+            GL.GetInteger(GetPName.MaxDrawBuffers, out queryinfo[2]);
+            GL.GetInteger(GetPName.Stereo, out queryinfo[3]);
+            GL.GetInteger(GetPName.Samples, out queryinfo[4]);
+            GL.GetInteger(GetPName.Doublebuffer, out queryinfo[5]);
+            Console.WriteLine("max. ColorBuffers: " + queryinfo[0] + " max. AuxBuffers: " + queryinfo[1] + " max. DrawBuffers: " + queryinfo[2] +
+                               "\nStereo: " + queryinfo[3] + " Samples: " + queryinfo[4] + " DoubleBuffer: " + queryinfo[5]);
+
+            Console.WriteLine("Last GL Error: " + GL.GetError());
         }
 
         #endregion
+
     }
 }
